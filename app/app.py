6e84216b7d14e0a1d267f054edf41f66a89d9263
KochetkovAnd <kochetpwr@gmail.com>
2024-11-06 00:25:22 +0400

    Пробросил картинку до питона

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask,request, jsonify
+from PIL import Image
 from modelUse import generate_palette
 
 app = Flask(__name__)
@@ -6,6 +7,21 @@ app = Flask(__name__)
 @app.route("/generate", methods=['GET'])
 def generate():    
     return jsonify(generate_palette())
+
+@app.route("/generate-by-picture", methods=['POST'])
+def upload_file():
+    print("sdfsdfsd")
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request'}), 400
+    
+    file = request.files['file']
+    try:
+        image = Image.open(file.stream)
+        return ["000000", "000000", "000000", "000000", "000000"]
+    except Exception as e:
+        return []
+    
+    
 
 if (__name__) == "__main__":
     app.run(port=8081)
